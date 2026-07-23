3d67d2b6f63f248257dd29ae1ce26ac6fb670e79
Михаил <misha13022008@gmail.com>
2026-07-24 02:28:48 +0300

    test: keep phi edge example in SSA form

--- crates/ir/tests/phi_cycle_example.rs
+++ crates/ir/tests/phi_cycle_example.rs
@@ -3,105 +3,92 @@ use ir::{Block, BlockId, Function, Inst, Terminator, VReg};
 use smallvec::smallvec;
 
 #[test]
-fn build_phi_cycle_function_parses_and_interps() -> Result<()> {
-    // Build a tiny CFG:
-    // entry:
-    //   v0 = arg0
-    //   v1 = arg1
-    //   cond = (v0 > v1)
-    //   br cond then else
-    // then: jmp join
-    // else: jmp join
-    // join:
-    //   x = phi(then: v1, else: v1)   // x=b
-    //   y = phi(then: v0, else: v0)   // y=a
-    //   ret y - x  (=> a-b)
-    //
-    // This isn't a true swap-cycle, but it forces φ moves for both values on edges.
-    // (Cycle tests are harder without explicit parallel move; this still stresses edge moves.)
+fn build_phi_edge_function_interprets() -> Result<()> {
     let entry = BlockId(0);
     let then_bb = BlockId(1);
     let else_bb = BlockId(2);
     let join = BlockId(3);
 
-    let v0 = VReg(0);
-    let v1 = VReg(1);
-    let c = VReg(2);
-    let x = VReg(3);
-    let y = VReg(4);
-    let negx = VReg(5);
-    let res = VReg(6);
+    let left = VReg(0);
+    let right = VReg(1);
+    let condition = VReg(2);
+    let phi_right = VReg(3);
+    let phi_left = VReg(4);
+    let negative_one = VReg(5);
+    let negative_right = VReg(6);
+    let result = VReg(7);
 
-    let b0 = Block {
+    let entry_block = Block {
         id: entry,
         insts: vec![
             Inst::ArgI64 {
-                dst: v0,
+                dst: left,
                 idx: ir::ArgId(0),
             },
             Inst::ArgI64 {
-                dst: v1,
+                dst: right,
                 idx: ir::ArgId(1),
             },
             Inst::CmpGtI64 {
-                dst: c,
-                a: v0,
-                b: v1,
+                dst: condition,
+                a: left,
+                b: right,
             },
         ],
         term: Terminator::Br {
-            cond: c,
+            cond: condition,
             then_bb,
             else_bb,
         },
     };
-    let b1 = Block {
+    let then_block = Block {
         id: then_bb,
         insts: vec![],
         term: Terminator::Jmp { target: join },
     };
-    let b2 = Block {
+    let else_block = Block {
         id: else_bb,
         insts: vec![],
         term: Terminator::Jmp { target: join },
     };
-    let b3 = Block {
+    let join_block = Block {
         id: join,
         insts: vec![
             Inst::PhiI64 {
-                dst: x,
-                incoming: smallvec![(then_bb, v1), (else_bb, v1)],
+                dst: phi_right,
+                incoming: smallvec![(then_bb, right), (else_bb, right)],
             },
             Inst::PhiI64 {
-                dst: y,
-                incoming: smallvec![(then_bb, v0), (else_bb, v0)],
+                dst: phi_left,
+                incoming: smallvec![(then_bb, left), (else_bb, left)],
             },
-            Inst::ConstI64 { dst: negx, imm: -1 },
+            Inst::ConstI64 {
+                dst: negative_one,
+                imm: -1,
+            },
             Inst::MulI64 {
-                dst: negx,
-                a: negx,
-                b: x,
+                dst: negative_right,
+                a: negative_one,
+                b: phi_right,
             },
             Inst::AddI64 {
-                dst: res,
-                a: y,
-                b: negx,
+                dst: result,
+                a: phi_left,
+                b: negative_right,
             },
         ],
-        term: Terminator::Ret { value: res },
+        term: Terminator::Ret { value: result },
     };
 
-    let f = Function {
+    let function = Function {
         name: "phi_edge_moves".into(),
         args: 2,
         entry,
-        blocks: vec![b0, b1, b2, b3],
+        blocks: vec![entry_block, then_block, else_block, join_block],
     };
 
-    let interp = ir::interp::Interpreter::default();
-    let r = interp.eval_i64(&f, &[10, 7])?;
-    assert_eq!(r, 3);
-    let r = interp.eval_i64(&f, &[7, 10])?;
-    assert_eq!(r, -3);
+    let interpreter = ir::interp::Interpreter;
+    assert_eq!(interpreter.eval_i64(&function, &[10, 7])?, 3);
+    assert_eq!(interpreter.eval_i64(&function, &[7, 10])?, -3);
     Ok(())
 }
